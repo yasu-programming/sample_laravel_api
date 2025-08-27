544005cf352f7691446f8ad7865b73d1dc1a792b
yasunori1005 <suzukiyasunori1005@gmail.com>
2025-08-27 13:30:32 +0900

    return hello

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/hello', function () {
+    return 'hello';
+});
